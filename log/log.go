56b2d11ee5617b0d09775d34970355d505266e7f
BingBingStudent <94032383+bing-bing-student@users.noreply.github.com>
2024-03-13 20:32:35 +0800

    Update log.go

--- log/log.go
+++ log/log.go
@@ -9,10 +9,11 @@ import (
 )
 
 type Logger interface {
+	Debug(v ...interface{})
 	Error(v ...interface{})
 	Warn(v ...interface{})
 	Info(v ...interface{})
-	Debug(v ...interface{})
+
 	
 	ErrorFormat(format string, v ...interface{})
 	WarnFormat(format string, v ...interface{})
